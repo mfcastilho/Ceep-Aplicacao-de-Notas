743857886403751359285fe2e48a5fa3a96a1be3
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-09-22 01:38:53 -0300

    criando função criarTarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,10 +1,15 @@
-const novaTarefa = document.querySelector('[data-form-button]');
 
-
-novaTarefa.addEventListener("click", (event)=> {
-  
+//Função de criar nova tarefa
+const criarTarefa = () =>{
   //o preventDefault() significar prevenir o comportamento padrão
   event.preventDefault();
   const formulario = document.querySelector('[data-form-input]');
   alert(formulario.value);
-});
+  formulario.value = "";
+}
+
+//Buscando botão Novo Item
+const novaTarefa = document.querySelector('[data-form-button]');
+
+//Evento do botão novaTarefa
+novaTarefa.addEventListener("click", criarTarefa);
